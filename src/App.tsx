a5148f03bd88c0470293ba30107ae755458aa7f1
Yuliya <yulia.podd@gmail.com>
2021-05-05 18:23:19 +0300

    init: start todo app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar } from './components/Navbar';
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Navbar />
+    <h1>Hello ts</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Navbar: React.FC = () => (
+  <nav>
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">
+        REACT+TS
+      </a>
+      <ul className="right hide-on-med-and-down">
+        <li>
+          <a href="/">Todo</a>
+        </li>
+        <li>
+          <a href="/">Info</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
