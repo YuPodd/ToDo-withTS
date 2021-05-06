779a62ff883d372022d3ae10d23ce8d00fc0b9a9
Yuliya <yulia.podd@gmail.com>
2021-05-06 15:27:55 +0300

    feat: add event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React,{useState} from 'react';
-import { Navbar } from './components/Navbar';
-import { TodoForm } from './components/TodoForm';
-import { TodoList } from './components/TodoList';
-import { ITodo } from './interfaces';
+import React, { useState } from "react";
+import { Navbar } from "./components/Navbar";
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { ITodo } from "./interfaces";
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -11,19 +11,37 @@ const App: React.FC = () => {
     const newTodo: ITodo = {
       title: title,
       id: Date.now(),
-      completed: false
-    }
-    setTodos(prev => [newTodo, ...prev]);
-  }
+      completed: false,
+    };
+    setTodos((prev) => [newTodo, ...prev]);
+  };
+  const toggleHandler = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const removeHandler = (id: number) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
   return (
     <>
-    <Navbar />
-    <div className="container">
-      <TodoForm onAdd={addHandler}/>
-      <TodoList todos={todos}/>
-    </div>
+      <Navbar />
+      <div className="container">
+        <TodoForm onAdd={addHandler} />
+        <TodoList
+          todos={todos}
+          onToggle={toggleHandler}
+          onRemove={removeHandler}
+        />
+      </div>
     </>
   );
-}
+};
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,22 +3,37 @@ import { ITodo } from "../interfaces";
 
 type TodoListProps = {
   todos: ITodo[];
+  onToggle(id: number): void;
+  onRemove: (id: number) => void;
 };
 
-export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onRemove,
+  onToggle,
+}) => {
   return (
     <ul>
       {todos.map((todo) => {
-          const classes = ['todo'];
-          if(todo.completed){
-              classes.push('completed')
-          }
+        const classes = ['todo'];
+        if (todo.completed) {
+          classes.push('completed');
+        }
         return (
           <li className={classes.join(' ')} key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed}/>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={onToggle.bind(null, todo.id)}
+              />
               <span>{todo.title}</span>
-              <i className="material-icons red-text">delete</i>
+              <i
+                className="material-icons red-text"
+                onClick={() => onRemove(todo.id)}
+              >
+                delete
+              </i>
             </label>
           </li>
         );
